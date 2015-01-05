a39460147d95a0085d57fd8cf00a959cf1f75a0f
unknown <masatsugu.yamada@MW742WTDVIKRU8.dir.svc.accenture.com>
2015-01-05 17:54:19 +0900

    eshellの文字化け対策

--- init.el
+++ init.el
@@ -712,3 +712,14 @@
 ;; eshell alias
 (add-to-list 'eshell-command-aliases-list (list "ll" "ls -la"))
 (add-to-list 'eshell-command-aliases-list (list "rr" "rm -rf"))
+
+;; eshell•¶š‰»‚¯‘Îô
+(add-hook 'set-language-environment-hook 
+          (lambda ()
+            (when (equal "ja_JP.UTF-8" (getenv "LANG"))
+             (setq default-process-coding-system '(utf-8 . utf-8))
+             (setq default-file-name-coding-system 'utf-8))
+            (when (equal "Japanese" current-language-environment)
+             (setq default-buffer-file-coding-system 'iso-2022-jp))))
+
+(set-language-environment "Japanese")
