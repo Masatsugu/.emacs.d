0fff723700d65ed7a62f2c314068a2e4644e67cd
Yamada Masatsugu <yamada.masatsugu@gmail.com>
2015-01-08 00:25:40 +0900

    create sql snippets

--- init.el
+++ init.el
@@ -559,7 +559,24 @@
 (define-key yas-minor-mode-map (kbd "C-x y n") 'yas-new-snippet)
 ;; 既存スニペットの閲覧、編集
 (define-key yas-minor-mode-map (kbd "C-x y v") 'yas-visit-snippet-file)
- 
+
+;; yasnippetをanythingインタフェースで選択する
+(defun my-yas/prompt (prompt choices &optional display-fn)
+  (let* ((names (loop for choice in choices
+                      collect (or (and display-fn (funcall display-fn choice))
+                                  coice)))
+         (selected (anything-other-buffer
+                    `(((name . ,(format "%s" prompt))
+                       (candidates . names)
+                       (action . (("Insert snippet" . (lambda (arg) arg))))))
+                    "*anything yas/prompt*")))
+    (if selected
+        (let ((n (position selected names :test 'equal)))
+          (nth n choices))
+      (signal 'quit "user quit!"))))
+(custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
+
+
 ;; anything interface
 (eval-after-load "anything-config"
   '(progn
